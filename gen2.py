a300e99074b4fab1cbafaf93f333573e33a03a3a
Ondřej Čertík <ondrej@certik.us>
2020-03-02 21:42:41 -0700

    Improve formatting further

--- gen2.py
+++ gen2.py
@@ -1,12 +1,15 @@
 f = open("hex1.lst").readlines()
 for line in f:
+    comment = line[40:-1]
     if line[8] == " ":
-        code2 = ""
+        if comment.strip() == "":
+            print()
+        else:
+            print("# %s" % (comment))
     else:
         code = line[16:40].strip().lower()
         code2 = ""
         for i in range(len(code)//2):
             code2 += code[2*i:2*i+2] + " "
         code2 = code2.strip()
-    comment = line[40:-1]
-    print("%-16s # %s" % (code2, comment))
+        print("    %-16s # %s" % (code2, comment))
