ee5b8695c668c9abeaf2dd7f57576523e9861daa
Ondřej Čertík <ondrej@certik.us>
2020-03-02 21:39:31 -0700

    Better formatting

--- hex1.asm
+++ hex1.asm
@@ -1,37 +1,37 @@
-  BITS 32
+BITS 32
 
-  origin        equ     0x08048000
+origin equ 0x08048000
 
-  ehdr:                                                 ; Elf32_Ehdr
-                db      0x7F, "ELF", 1, 1, 1, 0         ;   e_ident
-                db      0, 0, 0, 0, 0, 0, 0, 0
-                dw      2                               ;   e_type
-                dw      3                               ;   e_machine
-                dd      1                               ;   e_version
-                dd      _start - $$ + origin            ;   e_entry
-                dd      phdr - $$                       ;   e_phoff
-                dd      0                               ;   e_shoff
-                dd      0                               ;   e_flags
-                dw      ehdrsize                        ;   e_ehsize
-                dw      phdrsize                        ;   e_phentsize
-                dw      1                               ;   e_phnum
-                dw      0                               ;   e_shentsize
-                dw      0                               ;   e_shnum
-                dw      0                               ;   e_shstrndx
+ehdr:                       ; Elf32_Ehdr
+    db 0x7F, "ELF", 1, 1, 1 ; e_ident
+    db 0, 0, 0, 0, 0, 0, 0, 0, 0
+    dw 2                    ; e_type
+    dw 3                    ; e_machine
+    dd 1                    ; e_version
+    dd _start - $$ + origin ; e_entry
+    dd phdr - $$            ; e_phoff
+    dd 0                    ; e_shoff
+    dd 0                    ; e_flags
+    dw ehdrsize             ; e_ehsize
+    dw phdrsize             ; e_phentsize
+    dw 1                    ; e_phnum
+    dw 0                    ; e_shentsize
+    dw 0                    ; e_shnum
+    dw 0                    ; e_shstrndx
 
-  ehdrsize      equ     $ - ehdr
+ehdrsize equ $ - ehdr
 
-  phdr:                                                 ; Elf32_Phdr
-                dd      1                               ;   p_type
-                dd      0                               ;   p_offset
-                dd      origin                          ;   p_vaddr
-                dd      origin                          ;   p_paddr
-                dd      filesize                        ;   p_filesz
-                dd      filesize                        ;   p_memsz
-                dd      5                               ;   p_flags
-                dd      0x1000                          ;   p_align
+phdr:                       ; Elf32_Phdr
+    dd 1                    ; p_type
+    dd 0                    ; p_offset
+    dd origin               ; p_vaddr
+    dd origin               ; p_paddr
+    dd filesize             ; p_filesz
+    dd filesize             ; p_memsz
+    dd 5                    ; p_flags
+    dd 0x1000               ; p_align
 
-  phdrsize      equ     $ - phdr
+phdrsize equ $ - phdr
 
 
 _start:
@@ -90,4 +90,4 @@ exit:
     inc eax
     int 0x80
 
-filesize      equ     $ - $$
+filesize equ $ - $$
